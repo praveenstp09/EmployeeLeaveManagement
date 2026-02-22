43d60fc734ef44d2d3238b4f9be12c898ca9c1f2
Ashutosh Singh <asks.it10@gmail.com>
2026-02-22 16:57:40 +0530

    mychanges

--- Middlewares/JwtMiddleware.cs
+++ Middlewares/JwtMiddleware.cs
@@ -1,6 +1,30 @@
-﻿namespace EmpLeave.Middlewares
+﻿using EmpLeave.Services;
+
+namespace EmpLeave.Middlewares
 {
     public class JwtMiddleware
     {
+        private readonly RequestDelegate _next;
+
+        public JwtMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context, TokenService tokenService)
+        {
+            var token = context.Request.Headers["token"].FirstOrDefault();
+
+            if (!string.IsNullOrEmpty(token))
+            {
+                var userId = tokenService.ValidateToken(token);
+                if (userId != null)
+                {
+                    context.Items["UserId"] = userId;
+                }
+            }
+
+            await _next(context);
+        }
     }
 }
